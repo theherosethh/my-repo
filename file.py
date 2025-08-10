d49a888b4efc858430a5cac9606c1d3cdf37a359
seth <theherseth123456@gmail.comm>
2025-08-10 12:38:11 +0700

    First init project

---
+++ file.py
@@ -0,0 +1,1 @@
+print("Hello my name is \"Keo PanhaSeth\" \nPurpose of Learning Python \n \t - 1 I want to learn new skill \n \t - 2 I want to know about programming \n \t - 3 I want to be a software developer \n \nFrom \"IT Step Academy - Python Core\"")
---
+++ helloworld.py
@@ -0,0 +1,3 @@
+# print("Cooking Egg \n \t step-1: Heat the pan \n \t step-2: Add oil \n \t step-3: Break egg into pan \n \t step-4: Add seasoning \n \t step-5: wait \n \t step-6: Transfer")
+print("hello world \"hi world\"")
+print("hello world \\hi world\\")
