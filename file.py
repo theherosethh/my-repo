e3df9b6119a096e3124585a101a82dd2e1420d87
theherosethh <theheroseth123456@gmail.com>
2025-08-22 22:00:09 +0700

    Delete file.py

--- file.py
+++
@@ -1,2 +0,0 @@
-print("Hello my name is \"Keo PanhaSeth\" \nPurpose of Learning Python \n \t - 1 I want to learn new skill \n \t - 2 I want to know about programming \n \t - 3 I want to be a software developer \n \nFrom \"IT Step Academy - Python Core\"")
-Delete file.py
