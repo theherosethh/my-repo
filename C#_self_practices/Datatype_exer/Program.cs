cafb3cc0e8c44564c889b8c3f4391c49738dbe48
Panhaseth Keo <PanhaSeth095@outlook.com>
2025-08-27 23:38:31 +0700

    Added C# self practices

---
+++ C#_self_practices/Datatype_exer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[]args)
+    {
+        /*
+        Console.WriteLine("This is about employee details");
+
+        Console.Write("Your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Your department: ");
+        string department = Console.ReadLine();
+        Console.Write("Your address: ");
+        string address = Console.ReadLine();
+        Console.Write("Your salary: ");
+        int salary = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEmployee Details:");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Department: {department}");
+        Console.WriteLine($"Address: {address}");
+        Console.WriteLine($"Salary: {salary}");
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+        */
+
+        // Mark and score
+        //Console.WriteLine("Enter student's details");
+
+        //Console.Write("\nEnter registeration number: ");
+        //int regis = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter your name: ");
+        //string name = Console.ReadLine();
+
+        //Console.WriteLine("\nEnter mark of this subject");
+        //Console.WriteLine("Subject 1");
+        //int sub1 = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Subject 2");
+        //int sub2 = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Subject 3");
+        //int sub3 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("\nStudent Details as follows");
+        //Console.WriteLine($"Registeration Number: {regis}");
+        //Console.WriteLine($"Name: {name}");
+
+        //int sum = sub1 + sub2 + sub3;
+        //int average = sum / 3;
+        //Console.WriteLine($"Total Marks: {sum}");
+        //Console.WriteLine($"Average Marks: {average}");
+        //Console.ReadKey();
+
+        // This is about Operators
+        //bool result;
+        //result = (5 == 5);
+        //bool result1;
+        //result1 = (5 != 3);
+        //Console.WriteLine(result1);
+
+        //Arithmathic operator
+        //int num;
+        //int num1 = 2;
+        //int num2 = 4;
+        //num = num1 + num2;
+        //Console.WriteLine(num);
+
+        //Logical operators
+        bool a = true, b = false, c, d, e;
+        c = a && b; // (&&) is the (and) operator which return true when both condition is true.
+        d = a || b; // (||) is the (or) operator which return true when one or both conditin is true.
+        e = !b; // (!) is the logincal (not) operator which return true when the variable is false and reture false when the variable is true.
+        Console.WriteLine(e);
+
+
+
+
+
+    }
+}
