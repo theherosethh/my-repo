096ad1bf68f0ed821a0ceb1d1b782eed16cbafa5
Panhaseth Keo <PanhaSeth095@outlook.com>
2025-08-31 14:10:45 +0700

    changes in self_learning file

---
+++ C#_self_practices/Datatype_exer/Condition.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatype_exer
+{
+    internal class Switch_Statement
+    {
+        static void Main(string[] args)
+        {
+            //Usage:
+            //**Use when if-else statement is too complex and unreadable, also it uses to switch to any statement without it have to excute line by line.
+            //**It operate the same as if-else statement as it check the condition line by line whether it fullfills or skip the condition.
+            //Syntax:
+            //switch(variable)
+            //{
+            //case1
+            //execute your code
+            //break;
+            //case2:
+            //execute your code
+            //break;
+            //default:
+            //execute your code 
+            //break;
+            //}
+
+            // EXAMPLE_1: 
+
+            //int x = 0;
+            //switch (x)
+            //{
+            //    case 1:
+            //        Console.WriteLine("the choice is 1");
+            //    break;
+            //    case 2:
+            //        Console.WriteLine("the choice is 2");
+            //    break;
+            //    default:
+            //        Console.WriteLine("Invalid choice");
+            //    break;
+            //}
+
+            string str = "Java";
+            switch (str)
+            {
+                case "C#":
+                case "Java":
+                case "Python":
+                    Console.WriteLine("it's program");
+                break;
+
+                case "Mysql":
+                case "Oracle":
+                    Console.WriteLine("it's database");
+                break;
+                default:
+                    Console.WriteLine("Invalid");
+                break;
+            }
+
+
+
+
+
+        }
+      
+
+
+    }
+
+}
--- C#_self_practices/Datatype_exer/Program.cs
+++ C#_self_practices/Datatype_exer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -70,12 +71,64 @@ internal class Program
         //Console.WriteLine(num);
 
         //Logical operators
-        bool a = true, b = false, c, d, e;
-        c = a && b; // (&&) is the (and) operator which return true when both condition is true.
-        d = a || b; // (||) is the (or) operator which return true when one or both conditin is true.
-        e = !b; // (!) is the logincal (not) operator which return true when the variable is false and reture false when the variable is true.
-        Console.WriteLine(e);
+        //bool a = true, b = false, c, d, e;
+        //c = a && b; // (&&) is the (and) operator which return true when both condition is true.
+        //d = a || b; // (||) is the (or) operator which return true when one or both conditin is true.
+        //e = !b; // (!) is the logincal (not) operator which return true when the variable is false and reture false when the variable is true.
+        //Console.WriteLine(e);
+        //Control flow statement (if, else, goto statement, loop)
+        //if else in c#
 
+        //if (3 > 5)
+        //{
+        //    Console.WriteLine("20 is greather than 5");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("20 is not greater than 5");
+        //}
+
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Console.WriteLine("i is 5 times");
+        //}
+
+        //Console.WriteLine("this is statement1");
+        //goto statement3; //call statement (but have to declare it down first)
+        //Console.WriteLine("this is statement2");
+
+        //statement3:  //declare statement
+        //Console.WriteLine("this is statement3");
+
+
+        //int number;
+        //Console.WriteLine("Enter a Number: ");
+        //number = Convert.ToInt32(Console.ReadLine());
+        ////if (number > 10)
+        ////{
+        ////    Console.WriteLine($"{number} is greater than 10 ");
+        ////    Console.WriteLine("End of if block");
+        //}
+        //Console.WriteLine("Enter your age: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+        //if (number > 17)
+        //{
+        //    Console.WriteLine($"{number} you are a teenager");
+        //}
+        //else if (number <= 17 && number >= 16)
+        //{
+        //    Console.WriteLine($"{number} you are not a teenager");
+        //}
+        //else if (number <= 15 && number >= 14)
+        //{
+        //    Console.WriteLine($"{number} you are not qualify");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"{number} you are not here");
+        //}
+
+      
 
 
 
