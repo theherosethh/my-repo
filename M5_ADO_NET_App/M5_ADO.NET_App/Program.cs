6d33d2883fb5fcb983f0d6615a4ee9d7ad4dd8e7
PanhaSeth <theheroseth123456@gmail.com>
2025-10-29 08:13:48 -0700

    ado files

---
+++ M5_ADO_NET_App/M5_ADO.NET_App/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+
+namespace M5_ADO.NET_App
+{
+    class Program
+    {
+        static void User_main()
+        {
+           
+            bool backToMain = false;
+
+            while (!backToMain)
+            {
+                Console.WriteLine("-----User Management-----");
+                Console.WriteLine("1. View User");
+                Console.WriteLine("2. Add User");
+                Console.WriteLine("3. Edit User");
+                Console.WriteLine("4. Delete User");
+                Console.WriteLine("0. Back to Main Menu");
+                Console.WriteLine("--------------------------");
+                Console.Write("Select an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Users users = new Users();
+                        users.ViewUser();
+                        //users.ConnectSQL();
+                        
+                        break;
+                    case "2":
+                        Console.WriteLine("Add User");
+                        break;
+                    case "3":
+                        Console.WriteLine("Edit User");
+                        break;
+                    case "4":
+                        Console.WriteLine("Delete User");
+                        break;
+                    case "0":
+                        backToMain = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Press Enter to try again...");
+                        Console.ReadLine();
+                        break;
+                }
+
+                if (!backToMain)
+                {
+                    Console.WriteLine("Press Enter to continue...");
+                    Console.ReadLine();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool exitProgram = false;
+
+            while (!exitProgram)
+            {
+                Console.Clear();
+                Console.WriteLine("==== MAIN MENU ====");
+                Console.WriteLine("1. User Management");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select an option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        User_main();
+                        break;
+                    case "0":
+                        exitProgram = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Press Enter to try again...");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ M5_ADO_NET_App/M5_ADO.NET_App/Users.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace M5_ADO.NET_App
+{
+    public class Users
+    {
+        
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        string cs = "Server=DESKTOP-JA85VTV;Database=STEP_DB;User Id=sa;Password=123";
+        string id, user, pwd, role;
+        //Check connection Establish
+        /*
+        public void ConnectSQL()
+        {
+            cn = new SqlConnection(cs);
+            cn.Open();
+            if (cn.State == System.Data.ConnectionState.Open) 
+            {
+                Console.WriteLine("Connected");
+            }
+            cn.Close();
+        }
+        */
+        public void ViewUser()
+        {
+            cn = new SqlConnection(cs);
+            cn.Open();
+            cmd = new SqlCommand("SELECT * FROM users", cn);
+            cn = cmd.Connection;
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                Console.WriteLine("User ID \t\t Username \t\t Password \t\t Role");
+                while (dr.Read()) 
+                {
+                    Console.WriteLine("{0}{1}{2}{3}", dr.GetString(0), dr.GetString(1), dr.GetString(2), dr.GetString(3));
+                }
+
+            }
+        }
+        public void AddUser()
+        {
+            Console.Write("Input your user Id: ");
+            id = Console.ReadLine();
+            Console.Write("Input your user User Name: ");
+            user = Console.ReadLine();
+            Console.Write("Input your user User Password: ");
+            pwd = Console.ReadLine();
+            Console.Write("Input your user role: ");
+            role = Console.ReadLine();
+
+
+            cn = new SqlConnection(cs);
+            cn.Open();
+            cmd = new SqlCommand("INSERT INTO users VALUES(@id @user, @pwd, @role)", cn);
+            cmd.Parameters.Add("@id", System.Data.SqlDbType.NChar, 10).Value = id;
+            cmd.Parameters.Add("@user", System.Data.SqlDbType.NChar, 10).Value = user;
+            cmd.Parameters.Add("@pwd", System.Data.SqlDbType.NChar, 10).Value = pwd;
+            cmd.Parameters.Add("@role", System.Data.SqlDbType.NChar, 10).Value = role;
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Insert was successful");
+        }
+        public void EditUser()
+        {
+
+        }
+    }
+}
